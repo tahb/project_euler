e54c6307e394ee43caba925727ee41c15bcccb33
Hippers <tomhipkin@gmail.com>
2014-11-19 23:42:44 +0000

    problem 1

--- ruby/1.rb
+++ ruby/1.rb
@@ -1,4 +1,1 @@
-require 'rubygems'
-require 'pry'
-(1..1000).to_a.inject([]){|array, n| array << n if ((n%5).zero? || (n%3).zero?); array }
-
+(1..999).to_a.inject([]){|array, n| array << n if ((n%5).zero? || (n%3).zero?); array }.inject(:+)
