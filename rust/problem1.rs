447fecb9624cacca231a9f27f54cc705b7a49629
Tom Hipkin <tomhipkin@gmail.com>
2015-01-07 11:57:17 +0000

    rust

---
+++ rust/problem1.rs
@@ -0,0 +1,15 @@
+fn main() {
+  let mut count = 0i;
+
+
+  while count < 10 {
+    count += 1;
+
+    println!("count is {}", count);
+
+    if i % 3 == 0 || i % 5 == 0 {
+      array << i
+    }
+
+  }
+}
